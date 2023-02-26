bd9678ea92001fb15be3d0d922204e36eb40ef20
Scott Hickey <scott.hickey@genesis-fs.com>
2023-02-26 16:12:39 -0600

    refactored search button into a function component

--- lib/gravie_web/live/search_live.ex
+++ lib/gravie_web/live/search_live.ex
@@ -61,26 +61,7 @@ defmodule GravieWeb.SearchLive do
                 aria-label="Search"
                 aria-describedby="button-addon1"
               />
-              <button
-                class="relative z-[2] flex items-center rounded-r bg-primary px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
-                type="button"
-                id="button-addon1"
-                data-te-ripple-init
-                data-te-ripple-color="light"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="grey"
-                  class="h-5 w-5"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </button>
+              <.search_button/>
               <div class="flex items-center justify-center">
                 <div
                   :if={@loading}
@@ -241,6 +222,26 @@ defmodule GravieWeb.SearchLive do
   end
 
   # FUNCTION COMPONENTS
+  def search_button(assigns) do
+    ~H"""
+    <button
+      class="relative z-[2] flex items-center rounded-r bg-primary px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
+      type="button"
+      id="button-addon1"
+      data-te-ripple-init
+      data-te-ripple-color="light"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="grey" class="h-5 w-5">
+        <path
+          fill-rule="evenodd"
+          d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    </button>
+    """
+  end
+
   def nav(assigns) do
     ~H"""
     <nav
